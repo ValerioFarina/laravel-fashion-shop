e414aa6ba1d1d88fceb240919806810e11a06c3e
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-19 15:08:43 +0100

    Creati partials + layout di base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Homepage</title>
+@section('page-title', 'Homepage')
 
-        <!-- Fonts -->
-
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    </head>
-    <body>
-        <h1>Hello</h1>
-    </body>
-</html>
+@section('content')
+    <h1>
+        Homepage
+    </h1>
+@endsection
